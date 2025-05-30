c210072a4143491f88715ab79822b245b2f57a3a
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-05-30 05:44:18 +0000

    change the logic of following

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -21,22 +21,25 @@ class FollowRequestsController < ApplicationController
     the_follow_request = FollowRequest.new
     the_follow_request.sender_id = params.fetch("query_sender_id")
     the_follow_request.recipient_id = params.fetch("query_recipient_id")
+    redirect_path = params.fetch("redirect_to", "/users/#{the_follow_request.recipient_id}")
 
     if the_follow_request.valid?
       the_follow_request.save
-      redirect_to("/users/#{the_follow_request.recipient_id}", { :notice => "Followed successfully." })
+      redirect_to(redirect_path, { :notice => "Followed successfully." })
     else
-      redirect_to("/users/#{the_follow_request.recipient_id}", { :alert => the_follow_request.errors.full_messages.to_sentence })
+      redirect_to(redirect_path, { :alert => the_follow_request.errors.full_messages.to_sentence })
     end
   end
+
 
 
   def destroy
     the_id = params.fetch("path_id")
     the_follow_request = FollowRequest.where({ :id => the_id }).at(0)
     recipient_id = the_follow_request.recipient_id
-    the_follow_request.destroy
+    redirect_path = params.fetch("redirect_to", "/users/#{recipient_id}")
 
-    redirect_to("/users/#{recipient_id}", { :notice  => "Unfollowed successfully." })
+    the_follow_request.destroy
+    redirect_to(redirect_path, { :notice => "Unfollowed successfully." })
   end
 end
